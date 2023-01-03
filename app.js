9950ecd9bf5518b49a3654d19678420017c9bfbb
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2023-01-03 01:09:43 -0300

    modificando para levantar el proyecto en render

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -18,5 +18,5 @@ app.get("/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server up en: ${'http://localhost:3000/'}`);
+  console.log(`Server up en: ${PORT}`);
 });
