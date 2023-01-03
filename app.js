cc98605742e53cf645a6e7706d5b0dc7c64f4849
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2023-01-03 00:55:46 -0300

    volviendo a crear repositorio mercadoLiebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up en: ${'http://localhost:3000/'}`);
+});
